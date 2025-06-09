de3693def3e615e22a27c2dbd686617521ab96a9
SongokuBlue <anhduc28052004@gmail.com>
2025-06-09 21:36:40 +0200

    finish data update ,set show in profile page

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -3,7 +3,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:line_awesome_flutter/line_awesome_flutter.dart';
 import 'package:flutter/services.dart';
 import 'package:firebase_database/firebase_database.dart';
-
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
@@ -12,28 +11,83 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-  final User? user = FirebaseAuth.instance.currentUser;
+  final User? user = FirebaseAuth.instance.currentUser; // lấy dữ liệu hiện tại của user đó
 
+  bool isEditing = false;
+
+  // Controllers để lưu trữ dữ liệu nhập
+  final TextEditingController firstNameController = TextEditingController();
+  final TextEditingController lastNameController = TextEditingController();
+  final TextEditingController ageController = TextEditingController();
+  final TextEditingController phoneController = TextEditingController();
+  final TextEditingController addressController = TextEditingController();
+  final TextEditingController descriptionController = TextEditingController();
+  String gender = "Male";
+
+  void initState() {
+    super.initState();
+    _loadUserData();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Row(
-          mainAxisAlignment: MainAxisAlignment.start, // Căn giữa icon và text
           children: [
-            Text("Profile", style: TextStyle(fontWeight: FontWeight.bold)),
+            const Text("Profile", style: TextStyle(fontWeight: FontWeight.bold)),
+            const SizedBox(width: 10),
+            const Icon(LineAwesomeIcons.user_check_solid, color: Colors.blueAccent),
+            const Spacer(),
+            IconButton(
+              icon: Icon(isEditing ? Icons.save : Icons.edit),
+                onPressed: () async {
+                  if (isEditing) {
+                    // Save dữ liệu
+                    final userId = user?.uid;
+                    if (userId == null) return;
 
-            SizedBox(width: 10),
+                    final userRef = FirebaseDatabase.instance.ref().child('patients').child(userId);
+                    final snapshot = await userRef.get();
 
-            Icon(LineAwesomeIcons.user_check_solid, color: Colors.blueAccent),
+                    final dataToSave = {
+                      'firstName': firstNameController.text,
+                      'lastName': lastNameController.text,
+                      'age': int.tryParse(ageController.text) ?? 0,
+                      'gender': gender.toLowerCase(), // vì lưu trong database là ko in hoa
+                      'phone': phoneController.text,
+                      'address': addressController.text,
+                      'description': descriptionController.text,
+                      'authId': userId,
+                    }; //data cần lưu
+
+                    if (snapshot.exists) { //có tồn tại thì update ko thì tạo mới
+                      await userRef.update(dataToSave);
+                    } else {
+                      await userRef.set(dataToSave);
+                    }
+
+                    setState(() {
+                      isEditing = false;
+                    });
+
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text("Profile saved successfully!")),
+                    );
+                  } else {
+                    // Bật chế độ chỉnh sửa
+                    setState(() {
+                      isEditing = true;
+                    });
+                  }
+                }
+            ),
           ],
         ),
-
       ),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Icon(Icons.account_circle, size: 100, color: Colors.grey),
+            const Icon(Icons.account_circle, size: 100, color: Colors.grey),
             const SizedBox(height: 10),
             Text(
               "Email: ${user?.email ?? 'Not available'}",
@@ -45,44 +99,55 @@ class _ProfilePageState extends State<ProfilePage> {
               child: Column(
                 children: [
                   TextFormField(
+                    controller: firstNameController,
                     decoration: const InputDecoration(
                       labelText: "First Name",
                       border: OutlineInputBorder(),
                       prefixIcon: Icon(LineAwesomeIcons.user),
                     ),
+                    enabled: isEditing,
                   ),
                   const SizedBox(height: 20),
                   TextFormField(
+                    controller: lastNameController,
                     decoration: const InputDecoration(
                       labelText: "Last Name",
                       border: OutlineInputBorder(),
                       prefixIcon: Icon(LineAwesomeIcons.user),
                     ),
+                    enabled: isEditing,
                   ),
                   const SizedBox(height: 20),
                   Row(
                     children: [
                       Expanded(
                         child: TextFormField(
+                          controller: ageController,
                           keyboardType: TextInputType.number,
-                          inputFormatters: [FilteringTextInputFormatter.digitsOnly,],
+                          inputFormatters: [FilteringTextInputFormatter.digitsOnly],
                           decoration: const InputDecoration(
-                            labelText: "Age", border: OutlineInputBorder(), prefixIcon: Icon(LineAwesomeIcons.heart),
+                            labelText: "Age",
+                            border: OutlineInputBorder(),
+                            prefixIcon: Icon(LineAwesomeIcons.heart),
                           ),
+                          enabled: isEditing,
                         ),
                       ),
-                      const SizedBox(width: 16), // Khoảng cách giữa hai ô nhập
+                      const SizedBox(width: 16),
                       Expanded(
                         child: DropdownButtonFormField<String>(
-                          value: "Male", // mặc định
+                          value: gender,
                           items: const [
                             DropdownMenuItem(value: "Male", child: Text("Male")),
                             DropdownMenuItem(value: "Female", child: Text("Female")),
                           ],
-                          onChanged: (value) {
-                            // Gán giá trị cho biến trạng thái nếu cần
-                            print("Gender selected: $value");
-                          },
+                          onChanged: isEditing
+                              ? (value) {
+                            setState(() {
+                              gender = value!;
+                            });
+                          }
+                              : null,
                           decoration: const InputDecoration(
                             labelText: "Gender",
                             border: OutlineInputBorder(),
@@ -93,24 +158,36 @@ class _ProfilePageState extends State<ProfilePage> {
                     ],
                   ),
                   const SizedBox(height: 20),
-                   TextFormField(
-                    keyboardType: TextInputType.number, inputFormatters: [FilteringTextInputFormatter.digitsOnly,], decoration: const InputDecoration(labelText: "Phone number", border: OutlineInputBorder(), prefixIcon: Icon(LineAwesomeIcons.phone_solid),),
+                  TextFormField(
+                    controller: phoneController,
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                    decoration: const InputDecoration(
+                      labelText: "Phone number",
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(LineAwesomeIcons.phone_solid),
+                    ),
+                    enabled: isEditing,
                   ),
                   const SizedBox(height: 20),
                   TextFormField(
+                    controller: addressController,
                     decoration: const InputDecoration(
                       labelText: "Address",
                       border: OutlineInputBorder(),
                       prefixIcon: Icon(LineAwesomeIcons.address_card),
                     ),
+                    enabled: isEditing,
                   ),
                   const SizedBox(height: 20),
                   TextFormField(
+                    controller: descriptionController,
                     decoration: const InputDecoration(
                       labelText: "Description",
                       border: OutlineInputBorder(),
                       prefixIcon: Icon(LineAwesomeIcons.smile),
                     ),
+                    enabled: isEditing,
                   ),
                 ],
               ),
@@ -119,5 +196,25 @@ class _ProfilePageState extends State<ProfilePage> {
         ),
       ),
     );
+  }
+  Future<void> _loadUserData() async {
+    final userId = user?.uid;
+    if (userId == null) return;
+
+    final userRef = FirebaseDatabase.instance.ref().child('patients').child(userId);// đi từ database lúc đầu vào sâu hơn
+    final snapshot = await userRef.get();
+    if (snapshot.exists) { //nếu có tồn tại thì hiện
+      final data = snapshot.value as Map<dynamic, dynamic>;
+      setState(() {
+        firstNameController.text = data['firstName'] ?? '';
+        lastNameController.text = data['lastName'] ?? '';
+        ageController.text = (data['age'] ?? '').toString();
+        String rawGender = (data['gender'] ?? 'Male').toString();
+        gender = rawGender[0].toUpperCase() + rawGender.substring(1).toLowerCase();
+        phoneController.text = data['phone'] ?? '';
+        addressController.text = data['address'] ?? '';
+        descriptionController.text = data['description'] ?? '';
+      });
+    }
   }
 }
